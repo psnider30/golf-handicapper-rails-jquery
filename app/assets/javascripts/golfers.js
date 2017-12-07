b8c4a9ae8d64fee8c7088df455d5b1b5ab255679
psnider30 <pbsnider@gmail.com>
2017-12-07 12:46:28 -0800

    Add code translating json responses from $.get('/gofers/:id.json') into js Golfer object including Golfer fn's ready, nextListener, getNext, success, and error. And stub out Golfer.prototype.renderGolfer

--- app/assets/javascripts/golfers.js
+++ app/assets/javascripts/golfers.js
@@ -2,24 +2,45 @@ function Golfer(attributes) {
   this.id = attributes.id;
   this.name = attributes.name;
   this.email = attributes.email
+  this.golfer_index = attributes.golfer_index
+  this.rounds = attributes.rounds
+}
+
+Golfer.success = function(json) {
+  var golfer = new Golfer(json);
+  // var showGolfer = golfer.renderGolfer;
+  console.log(golfer);
+}
+
+Golfer.error = function(response) {
+  console.log("Not working?", response)
 }
 
 Golfer.prototype.renderGolfer = function() {
-  
+
+}
+
+Golfer.nextListener = function() {
+  var $next = $(".btn-primary.next-golfer")
+  var nextId = parseInt($next.attr("data-id")) + 1;
+  $next.click( function() {
+    Golfer.getNext(nextId);
+  });
+}
+
+Golfer.getNext = function(nextId) {
+  $.get("/golfers/" + nextId + ".json", function(golfer) {
+  })
+  .success(Golfer.success)
+  .error(Golfer.error)
+}
+
+Golfer.ready = function() {
+  // select template html
+  // compile handlesbars temmplate
+  Golfer.nextListener()
 }
 
 $(".golfers.show").ready(function() {
-  var golfer;
-  var $next = $(".btn-primary.next-golfer")
-  $next.on("click", function() {
-    var nextId = parseInt($next.attr("data-id")) + 1;
-    $.get("/golfers/" + nextId + ".json", function(data) {
-      golfer = data;
-      getGolfer(golfer);
-    });
-  });
+  Golfer.ready()
 });
-
-function getGolfer(golfer) {
-  return golfer;
-}
--- app/serializers/golfer_serializer.rb
+++ app/serializers/golfer_serializer.rb
@@ -2,5 +2,4 @@ class GolferSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :golfer_index
 
   has_many :rounds
-  has_many :golf_course_comments
 end
